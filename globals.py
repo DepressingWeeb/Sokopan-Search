d7f380177fdadae12cfc21b4bd2410ee26c73ffd
Buu Pham <pphambuu@gmail.com>
2024-10-25 16:48:24 +0700

    split to files

---
+++ globals.py
@@ -0,0 +1,11 @@
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+GREEN = (0, 127, 0)
+BLUE = (0, 0, 200)
+RED = (200, 0, 0)
+PURPLE = (255, 0, 255)
+YELLOW = (255, 255, 51)
+AQUA = (127, 255, 212)
+CLOCK = pygame.time.Clock()

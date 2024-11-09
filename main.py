7fbbd7cd1c85b4a387117787ca689e97d2308c45
Buu Pham <pphambuu@gmail.com>
2024-11-09 20:24:12 +0700

    update heuristic

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import pygame
 
 from A_star import AStar
 from bfs import BFS
-from utils import input_txt_file
+from utils import input_txt_file, get_all_output
 from visulizer import  Visualizer
 from main_screen import MainScreen
 import sys
 
 if __name__ == "__main__":
-
     pygame.init()
     #MainScreen().run()
     grid_string = '''
@@ -93,17 +92,18 @@ if __name__ == "__main__":
         ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
     ]
 
-    board_inp,weight_list_inp = input_txt_file(r'levels/Text10.txt')
-
-    visualizer_instance = Visualizer(grid_2d,[34,2,43,12,3])
+    board_inp,weight_list_inp = input_txt_file(r'levels/input-08.txt')
+    visualizer_instance = Visualizer(board_inp,weight_list_inp)
     visualizer_instance.visualize()
+    '''
     time_taken = multiprocessing.Value('d',0)
     node = multiprocessing.Value('i',0)
     path = multiprocessing.Array(ctypes.c_char, 10000)
     stop_signal = multiprocessing.Event()
     a_star_instance = AStar(board_inp,weight_list=[1,1,1,1,1,1])
-    #res = a_star_instance.A_star(time_taken,node,path,stop_signal)
-    a_star_instance.preprocess()
-
+    res = a_star_instance.A_star(time_taken,node,path,stop_signal)
+    print(res)
+    '''
+    get_all_output(r'levels')
 
 
